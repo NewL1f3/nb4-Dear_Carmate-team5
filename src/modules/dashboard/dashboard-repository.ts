c117ed9007a736df06a9c1632b1e3cef7a8b0ab1
Son JuneYeong <sonj0407@naver.com>
2025-10-21 15:32:41 +0900

    [fix] try catch 에러 헨들링 로직 추가

--- src/modules/dashboard/dashboard-repository.ts
+++ src/modules/dashboard/dashboard-repository.ts
@@ -2,80 +2,110 @@ import prisma from '../../lib/prisma';
 
 class dashboardRepository {
   aggregatePrice = async (companyId: number, startDay: Date, lastDay: Date) => {
-    const monthlySalesMedium: any = await prisma.contract.aggregate({
-      _sum: {
-        contractPrice: true,
-      },
-      where: {
-        companyId,
-        createdAt: {
-          gte: startDay,
-          lt: lastDay,
+    try {
+      const monthlySalesMedium: any = await prisma.contract.aggregate({
+        _sum: {
+          contractPrice: true,
         },
-      },
-    });
-    return monthlySalesMedium;
+        where: {
+          companyId,
+          createdAt: {
+            gte: startDay,
+            lt: lastDay,
+          },
+        },
+      });
+      return monthlySalesMedium;
+    } catch (error) {
+      console.error(error);
+      throw new Error('데이터베이스 에러');
+    }
   };
 
   getProceedingContractCount = async (companyId: number) => {
-    const proceedingContractCount = await prisma.contract.count({
-      where: {
-        companyId,
-        status: {
-          in: ['carInspection', 'priceNegotiation', 'contractDraft'],
+    try {
+      const proceedingContractCount = await prisma.contract.count({
+        where: {
+          companyId,
+          status: {
+            in: ['carInspection', 'priceNegotiation', 'contractDraft'],
+          },
         },
-      },
-    });
-    return proceedingContractCount;
+      });
+      return proceedingContractCount;
+    } catch (error) {
+      console.error(error);
+      throw new Error('데이터베이스 에러');
+    }
   };
 
   getCompletedContractCount = async (companyId: number) => {
-    const completedContractsCount = await prisma.contract.count({
-      where: {
-        companyId,
-        status: 'contractSuccessful',
-      },
-    });
-    return completedContractsCount;
+    try {
+      const completedContractsCount = await prisma.contract.count({
+        where: {
+          companyId,
+          status: 'contractSuccessful',
+        },
+      });
+      return completedContractsCount;
+    } catch (error) {
+      console.error(error);
+      throw new Error('데이터베이스 에러');
+    }
   };
 
   getContractCountWithCarType = async (companyId: number) => {
-    const cars = await prisma.car.findMany({
-      where: { companyId },
-      select: {
-        id: true,
-        model: {
-          select: {
-            type: true,
+    try {
+      const cars = await prisma.car.findMany({
+        where: { companyId },
+        select: {
+          id: true,
+          model: {
+            select: {
+              type: true,
+            },
+          },
+          _count: {
+            select: {
+              contracts: true, // 각 Car별 계약 수
+            },
           },
         },
-        _count: {
-          select: {
-            contracts: true, // 각 Car별 계약 수
-          },
-        },
-      },
-    });
-    return cars;
+      });
+      return cars;
+    } catch (error) {
+      console.error(error);
+      throw new Error('데이터베이스 에러');
+    }
   };
 
   getPriceByModelId = async (companyId: number) => {
-    const carPriceByModelId = await prisma.car.groupBy({
-      by: ['modelId'],
-      _sum: {
-        price: true,
-      },
-      where: { companyId },
-    });
-    return carPriceByModelId;
+    try {
+      const carPriceByModelId = await prisma.car.groupBy({
+        by: ['modelId'],
+        _sum: {
+          price: true,
+        },
+        where: { companyId },
+      });
+      return carPriceByModelId;
+    } catch (error) {
+      console.error(error);
+      throw new Error('데이터베이스 에러');
+    }
   };
 
   findModelName = async (item: any) => {
-    const findedmodel = await prisma.model.findUnique({
-      where: { id: item.modelId },
-      select: { type: true },
-    });
-    return findedmodel;
+    try {
+      const findedmodel = await prisma.model.findUnique({
+        where: { id: item.modelId },
+        select: { type: true },
+      });
+      return findedmodel;
+    } catch (error) {
+      console.error(error);
+      throw new Error('데이터베이스 에러');
+    }
   };
 }
 
